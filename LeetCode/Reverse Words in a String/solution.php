5ef840a5ae7182141cc75adc1922ae2053162049
Adeonita dos Santos <adeonita.sousa@gmail.com>
2019-03-10 15:32:35 -0300

    Resolução de Reverse Word in a String

---
+++ LeetCode/Reverse Words in a String/solution.php
@@ -0,0 +1,20 @@
+<?php
+  include("../exibearray.php");
+
+  $frase = "O problema foi resolvido";
+  $fraseDividida = explode(" ", $frase);
+  $arrayResultante = array();  //Criação da variável array resultante
+  for($i = count($fraseDividida); $i >= 0; $i--){  //Percorre a string dividida na ordem decrescente ao mesmo tempo que preeneche o array resultante
+    $arrayResultante[] = $fraseDividida[$i];
+  }
+
+  echo "<h2>Dada a frase: $frase </h2>";
+  echo "<h2>A sua reversa é: ";
+
+  foreach ($arrayResultante as $value) {
+    echo $value." ";
+  }
+
+ echo "</h2>";
+
+ ?>
--- LeetCode/exibearray.php
+++ LeetCode/exibearray.php
@@ -1,7 +1,7 @@
 <?php
 function exibeArray($array){
   foreach ($array as $value){
-    echo $value ."<br>";
+    echo $value. " ";
   }
 }
 ?>
