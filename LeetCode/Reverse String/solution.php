ab4faacb60a8287139acdd30915b76997f3d669f
Adeonita dos Santos <adeonita.sousa@gmail.com>
2019-03-10 14:35:51 -0300

    Inserção de comentário

--- LeetCode/Reverse String/solution.php
+++ LeetCode/Reverse String/solution.php
@@ -1,17 +1,17 @@
 <?php
   include("../exibearray.php");
 
-  $word = "passarinho";
+  $word = "arara";
 
   function reverseString($string){
-    $stringDividida = str_split($string);
-    $arrayResultante = array();
-    for($i = strlen($string); $i >= 0; $i-- ){
+    $stringDividida = str_split($string); //Divide a palavra inicial num array
+    $arrayResultante = array();  //Criação da variável array resultante
+    for($i = strlen($string); $i >= 0; $i-- ){  //Percorre a string dividida na ordem decrescente ao mesmo tempo que preeneche o array resultante
       $arrayResultante[] = $stringDividida[$i];
     }
-    return implode($arrayResultante);
+    return implode($arrayResultante);  //Retorna o array resultante já reverso
   }
-  echo "<h1>String inicial: $word</h1>";
-  $reversed = reverseString($word);
+  echo "<h1>String inicial: $word</h1>"; //Exibe os textos no navegador
+  $reversed = reverseString($word); //Guarda a palavra reversa na variável reversed
   echo "<h1>String revertida: $reversed</h1>";
  ?>
