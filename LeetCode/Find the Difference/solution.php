40059cfb1692e6e200f3abd5082289d2e6e62898
Adeonita dos Santos <adeonita.sousa@gmail.com>
2019-03-10 00:32:42 -0300

    Inclusão de arquivos Find the Difference + Exibe Array

---
+++ LeetCode/Find the Difference/solution.php
@@ -0,0 +1,32 @@
+<?php
+  $a = "abcd";
+  $b = "abcde";
+
+  $vetA = str_split($a); //Divide a string
+  $vetB = str_split($b); //Divide a string
+
+  function excedentesArray($maiorArray, $menorArray){ //Função procura o excedente entre os arrays
+    $valoresExcedentes = array();
+    foreach ($maiorArray as $key => $value){ //Percorre o maior array
+      if( !in_array($value, $menorArray) ){ //verifica se o valor encontrado não está no menor array
+        $valoresExcedentes[] = $value;  //Se verdadeiro salva no array $valoresExcedentes
+      }
+    }
+    return $valoresExcedentes;
+  }
+
+  function exibeArray($array){
+    foreach ($array as $value){
+      echo $value;
+    }
+  }
+
+  echo "<h1>Dados dois arrays de strings a saber</h1>  <p> Primeiro: '$a' <br><br> Segundo: '$b'</p>";
+  if( count($vetA) > count($vetB) ){ //Seleciona o maior array e o percorre
+    echo "O valor excedente entre os arrays é: ";
+    exibeArray(excedentesArray($vetA, $vetB));
+  }else {
+    echo "O valor excedente entre os arrays é: ";
+    exibeArray(excedentesArray($vetB, $vetA));
+  }
+?>
---
+++ LeetCode/exibearray.php
@@ -0,0 +1,7 @@
+<?php
+function exibeArray($array){
+  foreach ($array as $value){
+    echo $value ."<br>";
+  }
+}
+?>
