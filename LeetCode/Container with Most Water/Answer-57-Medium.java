4f7ad09663edb7d0a006b2deda3fd47245d52ab1
Halisson Araújo <83023695+HalissonWesker@users.noreply.github.com>
2022-03-02 10:33:44 -0300

    New Solution

--- LeetCode/Container with Most Water/Answer-57-Medium.java
+++ LeetCode/Container with Most Water/Answer-57-Medium.java
@@ -1,26 +1,22 @@
 public class WaterInContainer {	
 	
-	public int Area(int[] height) {
-	      int length=height.length;
-	      int end=length-1; 
-	      int begin=0; 
-	      int max=0; 
-	      while(begin<end)
-	      {
-	        int result=(end-begin)* Math.min(height[begin], height[end]);
-	        if(result>max)
-	        {
-	          max=result; 
-	        }
-	        if(height[begin]< height[end])
-	        {
-	          begin++; 
-	        }else
-	        {
-	          end--; 
-	        }
-	      }
-	      return max;
-	    }
+	public int maxArea(int[] height) {
+	int max = 0;
+	int a1 - 0;
+	int a2 = height.length-1;
+	
+	while (a1 < a2){
+		if (heught[a1] ,height[a2]) {}
+			max = math.max(max, height[a1] * (a2 - a1));
+			a1 += 1;
+		} 
+		else {
+			max = Math.max(max, height[a2] * (a2-a1));
+			a2 -= 1;
+		}
+		return maxArea
+					}
+
 
 }
+
