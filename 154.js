1621d07a068c2c1ac448205ea67bf8389fc1b2c2
sandrofilhoo <sandrosouzafilho@hotmail.com>
2022-03-07 23:01:04 -0300

    change

--- 154.js
+++ 154.js
@@ -5,16 +5,18 @@ function run(){
 
   // [5,6,7,0,1,4,4]
 
-  var nums = [4,5,6,7,0,1,4]
+  const nums = [4,5,6,7,0,1,4]
 
-  var i = 0,
+  var i = 0
   var all = nums.length - 1;
   while (i < all) {
     var mid = i + (all - i) / 2;
+    console.log(mid)
     if (nums[mid] < nums[all]) all = mid;
-    else if (nums[mid] > nums[all]) i = mid + 1;
+    else if (nums[mid] > nums[all]) i = mid;
     else all--;
   }
-
-  return nums[i];
+  console.log(nums[i])
+  return console.log(nums[i]);
 }
+run()
